8757344067dfe9075cbd1b4d09afa1a889a6e307
MinexAutomation <minex.automation@gmail.com>
2020-07-05 00:29:57 -0700

    Added R5T.D0022.Default library. Added VisualStudioProjectFileXDocumentPrettifier service implementation class type. Added IServiceCollection extensions.

---
+++ source/R5T.D0022.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0022.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the see <see cref="VisualStudioProjectFileXDocumentPrettifier"/> implementation of <see cref="IVisualStudioProjectFileXDocumentPrettifier"/> as a s<see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioProjectFileXDocumentPrettifier(this IServiceCollection services)
+        {
+            services.AddSingleton<IVisualStudioProjectFileXDocumentPrettifier, VisualStudioProjectFileXDocumentPrettifier>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the see <see cref="VisualStudioProjectFileXDocumentPrettifier"/> implementation of <see cref="IVisualStudioProjectFileXDocumentPrettifier"/> as a s<see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IVisualStudioProjectFileXDocumentPrettifier> AddVisualStudioProjectFileXDocumentPrettifierAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<IVisualStudioProjectFileXDocumentPrettifier>(() => services.AddVisualStudioProjectFileXDocumentPrettifier());
+            return serviceAction;
+        }
+    }
+}
